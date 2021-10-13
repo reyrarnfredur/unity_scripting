8a2ae40ac323bbe98542688ee53be4af39e4c79e
reyrarnfredur <59200872+reyrarnfredur@users.noreply.github.com>
2021-10-13 20:46:17 +0200

    Add files via upload

---
+++ Utility/UtilityFunctions.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class Utility
+{
+
+    //scales number with range (min...max) to (0...1) and returns float value
+    private float ScaleFloat01(float n, float min, float max){
+        float r = (n - min) / (max - min);
+        return r;
+    }
+
+    
+
+}
