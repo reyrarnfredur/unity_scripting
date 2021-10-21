e6d6659f9bcce13e0460913ddcf13191e58dac4f
reyrarnfredur <59200872+reyrarnfredur@users.noreply.github.com>
2021-10-21 20:49:37 +0200

    Update UtilityFunctions.cs

--- Utility/UtilityFunctions.cs
+++ Utility/UtilityFunctions.cs
@@ -1,15 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-
 
 public static class Utility
 {
 
-    //scales number with range (min...max) to (0...1) and returns float value
-    public static float ScaleFloat01(float n, float min, float max){
+    public static float Normalize(float n, float min, float max){
         float r = (n - min) / (max - min);
         return r;
+    }
+    
+    public static float Interpolate(float a, float b, float t) {
+        float r = (b - a) * t + a;
+        return r;
+    }
+    
+    public static float Clamp(float n, float min, float max) {
+        return Mathf.Max(Mathf.Min(n, min), max);
+    }
+    
+    public static float Map(float n, float min, float max, float otherMin, float otherMax) {
+        float t = Normalize(n, min, max);
+        float r = Interpolate(otherMin, otherMax, t);
+    }
+    
+    public static Vector2 GetDirection(float angle) {
+        Vector3 v = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;
+        return v;
+    }
+    
+    public static float GetAngle(Vector2 direction) {
+        return Mathf.Atan2(direction.y, direction.x);
     }
 
 }
